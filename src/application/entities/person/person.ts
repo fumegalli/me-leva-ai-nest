adc238e05539dc10c5ca800d5073a364a93abd8a
Fumegalli <rafael.fumegalli@cwi.com.br>
2022-12-14 17:20:39 -0300

    refact(person): Extends new base entity

--- src/application/entities/person/person.ts
+++ src/application/entities/person/person.ts
@@ -1,35 +1,17 @@
-import { randomUUID } from 'node:crypto';
-import { Replace } from '../../helpers/Replace';
+import BaseEntity from '../base-entity';
 import Email from '../person/email';
 
 export interface PersonProps {
   fullName: string;
   email: Email;
   birthDate: Date;
-  createdAt: Date;
 }
 
-export default class Person {
-  private _id: string;
+export default class Person extends BaseEntity {
   private props: PersonProps;
 
-  constructor(props: Replace<PersonProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
-    this.props = {
-      ...props,
-      createdAt: props.createdAt ?? new Date(),
-    };
-  }
-
-  public get id(): string {
-    return this._id;
-  }
-
-  public get email(): string {
-    return this.props.email.value;
-  }
-
-  public get birthDate(): Date {
-    return this.props.birthDate;
+  constructor(props: PersonProps) {
+    super();
+    this.props = props;
   }
 }
